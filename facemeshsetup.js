ee515affa956f54330a8a575edfed5a21138dd1f
Dani Rocha Azevedo <azevedorocha.dani@gmail.com>
2023-10-04 13:32:29 +0200

    .

--- facemeshsetup.js
+++ facemeshsetup.js
@@ -6,11 +6,7 @@ async function setupCamera() {
     // Request the front-facing camera of the device
     const stream = await navigator.mediaDevices.getUserMedia({
         'audio': false,
-        'video': {
-            facingMode: 'user',
-            height: {ideal:1920},
-            width: {ideal: 1920},
-        },
+        'video': true,
     });
     video.srcObject = stream;
     // Handle the video stream once it loads.
