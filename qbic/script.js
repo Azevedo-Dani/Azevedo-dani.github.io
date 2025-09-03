d955947ef73f08535d094cb739fc97b34dfa56f6
Dani Rocha Azevedo <azevedorocha.dani@gmail.com>
2025-09-03 15:09:03 +0200

    play remote url

--- qbic/script.js
+++ qbic/script.js
@@ -1,8 +1,38 @@
-
 var log = function(message) {
     var container = document.getElementById('logs')
     var containerMessage = document.createElement('div')
     containerMessage.innerText = message
     container.appendChild(containerMessage)
 }
-log('START PLAY CONTENT')
+
+var startPlay = function (url) {
+    var body = {
+        pkgname: 'com.qbic.smilplayer',
+        classname: 'com.qbic.smilplayer.PlayerActivity',
+        action: 'android.intent.action.VIEW',
+        flags: ['0x10000000'], // FLAG_ACTIVITY_NEW_TASK
+        data: url
+    };
+
+    return fetch('http://localhost:8080/v1/task/start_app', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json; charset=utf-8' },
+        body: JSON.stringify(body),
+    }).then(function (start) {
+        if (!start.ok) {
+            throw new Error(`start_app failed: ${start.status}`)
+        }
+    })
+}
+
+var playFromRemote = function () {
+    startPlay('https://phil1618.github.io/event-27788-or-download.mp4').then(function() {
+        log('START PLAYING')
+    }).catch(function() {
+        log('ERROR PLAYING')
+    })
+}
+
+log('INIT')
+
+playFromRemote()
