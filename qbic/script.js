7164b685022d7661a527ecc7f801e82bf6a54919
Dani Rocha Azevedo <azevedorocha.dani@gmail.com>
2025-09-03 15:22:39 +0200

    wip

--- qbic/script.js
+++ qbic/script.js
@@ -7,12 +7,26 @@ var log = function(message) {
 
 var startPlay = function (url) {
     var body = {
-        pkgname: 'com.qbic.smilplayer',
-        classname: 'com.qbic.smilplayer.PlayerActivity',
-        action: 'android.intent.action.VIEW',
-        flags: ['0x10000000'], // FLAG_ACTIVITY_NEW_TASK
-        data: url
-    };
+        "pkgname":"com.qbic.smilplayer",
+        "classname":"com.qbic.smilplayer.PlayerActivity",
+        "data":"https://www.google.com",
+        "flag":["0x10000000", "0x00010000", "0x40000000"],
+        "action":"android.intent.action.MAIN",
+        "categories":["android.intent.category.DEFAULT",
+            "android.intent.category.LAUNCHER"],
+        "extras":
+            [
+                {
+                    "name":"extra_1",
+                    "type":"int",
+                    "data":["1", "3", "7", "0"]
+                },
+                {
+                    "name":"extra_2",
+                    "type":"float",
+                    "data":["23.555"]
+                }
+            ]};
 
     return fetch('http://localhost:8080/v1/task/start_app', {
         method: 'POST',
