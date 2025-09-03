d7c8fce9eb659ffbc571a75a58bfd150f5707083
Dani Rocha Azevedo <azevedorocha.dani@gmail.com>
2025-09-03 15:36:11 +0200

    wip

--- qbic/script.js
+++ qbic/script.js
@@ -14,19 +14,7 @@ var startPlay = function (url) {
         "action":"android.intent.action.MAIN",
         "categories":["android.intent.category.DEFAULT",
             "android.intent.category.LAUNCHER"],
-        "extras":
-            [
-                {
-                    "name":"extra_1",
-                    "type":"int",
-                    "data":["1", "3", "7", "0"]
-                },
-                {
-                    "name":"extra_2",
-                    "type":"float",
-                    "data":["23.555"]
-                }
-            ]};
+        };
 
     return fetch('http://localhost:8080/v1/task/start_app', {
         method: 'POST',
@@ -47,6 +35,6 @@ var playFromRemote = function () {
     })
 }
 
-log('INIT')
+log('INIT2')
 
 playFromRemote()
